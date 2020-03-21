1fa73244af8ac369dd549d9b1b10d7aea15d9525
Joakim Hellsén <tlovinator@gmail.com>
2020-03-21 23:25:23 +0100

    Add support for file uploads
    You can now upload files with --upload

--- main.py
+++ main.py
@@ -2,14 +2,39 @@ import argparse
 import datetime
 import os
 import random
+import shutil
 import subprocess
 import webbrowser
+
+
+def upload_file(source_file: str):
+    """
+    Copy file to location
+    """
+
+    file_name: str = os.path.basename(source_file)  # Remove path
+    folder: str = f"/mnt/wd_white/Nginx/www.lovinator/files/{now:%Y}/{now:%m}/"
+    destination: str = f"{folder}/{file_name}"
+    file_url: str = f"https://lovinator.xyz/files/{now:%Y}/{now:%m}/{file_name}"
+
+    # Check if path is working.
+    try:
+        create_folder(path=folder)
+    except Exception as e:
+        send_notification(f"Failed to create folder.\n{e}")
+
+    # Copy
+    shutil.copy(source_file, destination)
+    print(f"{file_name} - {source_file} copied to {destination}")
+
+    return file_url
 
 
 def append_url_to_file(url: str):
     """
     Append url to file.
     """
+    # TODO: Add file type
     append_time = datetime.datetime.now()
     with open("urls.txt", "a") as f:
         f.write(f"{append_time} - {url}\n")
@@ -18,8 +43,10 @@ def append_url_to_file(url: str):
 
 def send_notification(message: str):
     """
-    Send desktop notification.
+    Send desktop notification. Needs libnotify.
     """
+    # TODO: Add support for different levels
+    # TODO: Add image in notification
     subprocess.Popen(["notify-send", message])
     print(f"Message sent: {message}")
 
@@ -34,7 +61,7 @@ def random_char(amount: int):
     return "".join(random.choice(letters_and_digits) for x in range(amount))
 
 
-def create_folder():
+def create_folder(path: str):
     """
     Check if folder exists. If not, create one.
     """
@@ -47,22 +74,57 @@ def create_folder():
         return 0
 
 
-def screenshot():
-    # TODO: Check if name is already taken
+def clipboard(clipboard_string: str):
+    """
+    Add url to clipboard with xsel.
+    """
+    subprocess.Popen(
+        ("xsel", "--input", "--clipboard"),
+        stdin=subprocess.PIPE,
+        universal_newlines=True,
+    ).communicate(clipboard_string)
+
+
+def take_screenshot():
     """
     Screenshot and move to folder.
     """
-
-    # Check if path is working.
-    create_folder()
 
     if args.select:
         option = "--select"
     if args.focused:
         option = "--focused"
 
-    # Take the image
-    subprocess.run(["scrot", option, "--freeze", f"{path}/{filename}.png"])
+    image_filename: str = random_char(amount=10)
+    image_path: str = f"/mnt/wd_white/Nginx/www.lovinator/i/{now:%Y}/{now:%m}"
+    image_url: str = f"https://lovinator.xyz/i/{now:%Y}/{now:%m}/{image_filename}.png"
+
+    # Check if path is working.
+    try:
+        create_folder(path=image_path)
+    except Exception as e:
+        send_notification(f"Failed to create folder.\n{e}")
+
+    # Take the actual screenshot
+    try:
+        subprocess.run(
+            ["scrot", option, "--freeze", f"{image_path}/{image_filename}.png"]
+        )
+    except Exception as e:
+        send_notification(f"Failed to create folder.\n{e}")
+
+    return image_url
+
+
+def main():
+    # TODO: Only add to clipboard and open url if there actually is a image
+    # If image
+    if args.focused or args.select:
+        url = take_screenshot()
+
+    # If file
+    if args.upload:
+        url = upload_file(source_file=str(args.upload))
 
     # Send desktop notification
     send_notification(f"URL: {url}")
@@ -71,30 +133,14 @@ def screenshot():
     append_url_to_file(url=url)
 
     # Copy link to clipboard
-    clipboard(variable=url)
+    clipboard(clipboard_string=url)
 
     # Open link in browser
     webbrowser.open_new_tab(url=url)
 
 
-def clipboard(variable):
-    """
-    Add url to clipboard with xsel.
-    """
-    subprocess.Popen(
-        ("xsel", "--input", "--clipboard"),
-        stdin=subprocess.PIPE,
-        universal_newlines=True,
-    ).communicate(variable)
-
-
 if __name__ == "__main__":
     now = datetime.datetime.now()
-    # TODO: Different file types should go to different folders
-
-    filename: str = random_char(amount=10)
-    path: str = f"/mnt/wd_white/Nginx/www.lovinator/i/{now:%Y}/{now:%m}"
-    url: str = f"https://lovinator.xyz/i/{now:%Y}/{now:%m}/{filename}.png"
 
     # Command-line options, arguments and sub-commands
     parser = argparse.ArgumentParser(description="ShareX but cooler.")
@@ -114,6 +160,13 @@ if __name__ == "__main__":
         required=False,
         action="store_true",
     )
+    g.add_argument(
+        "-u",
+        "--upload",
+        help="upload a file. File will be replaced if already exist",
+        required=False,
+        action="store",
+    )
     args = parser.parse_args()
 
-    screenshot()
+    main()
